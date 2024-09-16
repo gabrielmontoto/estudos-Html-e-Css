609d956772cd67b1c04cb67a1e50351ce089fba3
gabrielmontoto <40693046+gabrielmontoto@users.noreply.github.com>
2024-09-16 14:43:13 -0300

    update javascript e curriculo

---
+++ javascripts/Template/assets/javascript/script.js
@@ -0,0 +1,8 @@
+let b = "";
+let v = `${b}`;
+
+// document.querySelectorAll()
+// document.querySelector('#idName')
+// document.querySelector('.className')
+// document.getElementsByTagName('div')
+// document.getElementById('divName')
